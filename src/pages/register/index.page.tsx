c343b8b81ca3f3eca03067264afcd2f833934e37
Henrique Pereira <henrique802011@gmail.com>
2023-02-13 22:06:33 -0300

    projeto-6: next-auth completo

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -48,6 +48,7 @@ export default function Register() {
         username: data.username,
         name: data.name,
       })
+      await router.push('/register/connect-calendar')
     } catch (err) {
       console.log(err)
     }
